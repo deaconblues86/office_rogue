19427394090b0a808c2f6f8d6d3efb107b25a946
Brian Hammill <hammillbc@gmail.com>
2020-03-07 14:00:58 -0500

    added a few item unit tests and turned pytest on in workflow

---
+++ tests/base_test.py
@@ -0,0 +1,29 @@
+import json
+from unittest import TestCase
+from unittest.mock import patch
+from base.game import GameInstance
+from base.map_gen import MapGenerator
+
+
+def message_patch(game, *args, **kwargs):
+    """
+    Intercepts messages that would normally be displayed through render
+     - Pretty much only called by BaseObject.broadcast()
+    """
+    return f"{','.join(args)} - {json.dumps(kwargs)}"
+
+
+class BaseTestCase(TestCase):
+    def setUp(self):
+        self.game = GameInstance()
+        self.map_gen = MapGenerator(self.game)
+
+        self.map_gen.generate_map()
+        self.map_gen.generate_path_map()
+
+        # self.broadcase_patch = patch("base.game.GameInstance.log_message", message_patch)
+        # self.broadcase_patch.start()
+
+    def tearDown(self):
+        pass
+        # self.broadcase_patch.stop()
---
+++ tests/test_appliances.py
@@ -0,0 +1,40 @@
+import pytest
+from tests.base_test import BaseTestCase
+from constants import game_objects
+
+
+class TestApplianceClass(BaseTestCase):
+    def setUp(self):
+        super().setUp()
+        self.appliance = self.game.create_object(0, 0, game_objects["T"])
+        self.user = self.game.create_object(0, 0, game_objects["Coworker"])
+
+    def test_not_broken(self):
+        self.appliance.eval_triggers()
+        self.assertTrue("repairing" not in self.game.work_requests)
+
+    def test_broken(self):
+        self.assertEqual(len(self.game.work_requests.keys()), 0)
+        # Might as well test that negatives work for these less or equals
+        self.appliance.durability = -1
+        self.appliance.eval_triggers()
+        self.assertTrue("repairing" in self.game.work_requests)
+
+    def test_not_dirty(self):
+        self.appliance.eval_triggers()
+        self.assertTrue("cleaning" not in self.game.work_requests)
+
+    def test_dirty(self):
+        self.assertEqual(len(self.game.work_requests.keys()), 0)
+        # Might as well test that negatives work for these less or equals
+        self.appliance.cleanliness = -1
+        self.appliance.eval_triggers()
+        self.assertTrue("cleaning" in self.game.work_requests)
+
+    def test_occupied(self):
+        # Name needs to be the same for mesage test
+        self.user.name = 'Natalie'
+        self.appliance.occupied_by = self.user
+
+        self.assertFalse(self.appliance.can_use(self.user))
+        self.assertEqual(self.game.game_msgs, [('Toilet: 0,0 occupied by Natalie', 'white')])
