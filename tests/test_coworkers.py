6be58c0cc5ab94a0ad6e2bfad4d1058aa7df625a
Brian Hammill <hammillbc@gmail.com>
2020-03-19 21:16:17 -0400

    uncommented coworker tests

--- tests/test_coworkers.py
+++ tests/test_coworkers.py
@@ -11,15 +11,15 @@ class TestCoworkerClass(BaseTestCase):
         self.coworker.name = "Test"
         print(self.coworker.job)
 
-    # def test_check_needs(self):
-    #     self.coworker.bladder = 5
-    #     self.coworker.check_needs()
-    #     self.assertEqual(self.coworker.satisfying, "bladder")
-    #     self.assertEqual(repr(self.coworker.target_action), "Action: peeing_in_toilet of Test at 0, 0")
-    #     self.assertEqual(
-    #         [x.split(" at ")[0] for x in self.game.game_debug_msgs],    # Need to normalize error message
-    #         ["Test can't path to Toilet"]
-    #     )
+    def test_check_needs(self):
+        self.coworker.bladder = 5
+        self.coworker.check_needs()
+        self.assertEqual(self.coworker.satisfying, "bladder")
+        self.assertEqual(repr(self.coworker.target_action), "Action: peeing_in_toilet of Test at 0, 0")
+        self.assertEqual(
+            [x.split(" at ")[0] for x in self.game.game_debug_msgs],    # Need to normalize error message
+            ["Test can't path to Toilet"]
+        )
 
     def test_action_weighting(self):
         """
@@ -38,95 +38,95 @@ class TestCoworkerClass(BaseTestCase):
         self.coworker.thirst = 5
         self.coworker.check_needs()
 
-    # def test_task_action_failure(self):
-    #     """
-    #     Test to verify that actions require associated tasks will not be performed unless the task actually exists
-    #     """
-    #     self.coworker.work = 5
-    #     self.coworker.job = "housekeeping"
-    #     self.coworker.action_center.true_up_actions()
-    #     self.coworker.check_needs()
+    def test_task_action_failure(self):
+        """
+        Test to verify that actions require associated tasks will not be performed unless the task actually exists
+        """
+        self.coworker.work = 5
+        self.coworker.job = "housekeeping"
+        self.coworker.action_center.true_up_actions()
+        self.coworker.check_needs()
 
-    #     # First attempt will be unavailable as nothing is dirty
-    #     self.assertEqual(self.coworker.satisfying, "work")
-    #     self.assertEqual(
-    #         [repr(x) for x in self.coworker.memories.unavailable_actions],
-    #         ["Action: cleaning of Test at 0, 0"]
-    #     )
-    #     self.assertEqual(self.coworker.target, None)
+        # First attempt will be unavailable as nothing is dirty
+        self.assertEqual(self.coworker.satisfying, "work")
+        self.assertEqual(
+            [repr(x) for x in self.coworker.memories.unavailable_actions],
+            ["Action: cleaning of Test at 0, 0"]
+        )
+        self.assertEqual(self.coworker.target, None)
 
-    #     # Second attempt will be unavailable as no "Trash" objects exist
-    #     self.coworker.check_needs()
-    #     self.assertEqual(self.coworker.satisfying, "work")
-    #     self.assertEqual(
-    #         [repr(x) for x in self.coworker.memories.unavailable_actions],
-    #         ["Action: cleaning of Test at 0, 0", "Action: cleaning_up of Test at 0, 0"]
-    #     )
-    #     self.assertEqual(self.coworker.target, None)
+        # Second attempt will be unavailable as no "Trash" objects exist
+        self.coworker.check_needs()
+        self.assertEqual(self.coworker.satisfying, "work")
+        self.assertEqual(
+            [repr(x) for x in self.coworker.memories.unavailable_actions],
+            ["Action: cleaning of Test at 0, 0", "Action: cleaning_up of Test at 0, 0"]
+        )
+        self.assertEqual(self.coworker.target, None)
 
-    #     # Third attempt may work if unowned terminals exist, otherwise
-    #     # computing will at least be tried.  In either case, no target will be acquired as,
-    #     # even if an available terminal exist, the test coworker can't path to it.
-    #     self.coworker.check_needs()
-    #     if any([x.owner is None for x in self.game.find_objs("Terminal")]):
-    #         self.assertEqual(repr(self.coworker.target_action), "Action: computing of Test at 0, 0")
-    #         self.assertEqual(self.coworker.target, None)
-    #     else:
-    #         self.assertEqual(
-    #             [repr(x) for x in self.coworker.memories.unavailable_actions],
-    #             [
-    #                 "Action: cleaning of Test at 0, 0",
-    #                 "Action: cleaning_up of Test at 0, 0",
-    #                 "Action: computing of Test at 0, 0"
-    #             ]
-    #         )
-    #         self.assertEqual(self.coworker.target, None)
+        # Third attempt may work if unowned terminals exist, otherwise
+        # computing will at least be tried.  In either case, no target will be acquired as,
+        # even if an available terminal exist, the test coworker can't path to it.
+        self.coworker.check_needs()
+        if any([x.owner is None for x in self.game.find_objs("Terminal")]):
+            self.assertEqual(repr(self.coworker.target_action), "Action: computing of Test at 0, 0")
+            self.assertEqual(self.coworker.target, None)
+        else:
+            self.assertEqual(
+                [repr(x) for x in self.coworker.memories.unavailable_actions],
+                [
+                    "Action: cleaning of Test at 0, 0",
+                    "Action: cleaning_up of Test at 0, 0",
+                    "Action: computing of Test at 0, 0"
+                ]
+            )
+            self.assertEqual(self.coworker.target, None)
 
-    # def test_task_action_success(self):
-    #     """
-    #     Test that once a task is actually created, the associated action will be performed
-    #     """
-    #     toilet = self.game.find_objs("Toilet")[0]
-    #     toilet.cleanliness = 0
-    #     toilet.eval_triggers()
+    def test_task_action_success(self):
+        """
+        Test that once a task is actually created, the associated action will be performed
+        """
+        toilet = self.game.find_objs("Toilet")[0]
+        toilet.cleanliness = 0
+        toilet.eval_triggers()
 
-    #     self.coworker.work = 5
-    #     self.coworker.job = "housekeeping"
-    #     self.coworker.action_center.true_up_actions()
+        self.coworker.work = 5
+        self.coworker.job = "housekeeping"
+        self.coworker.action_center.true_up_actions()
 
-    #     # In addtion to changing job, lets move the coworker somewhere within the level (top-right corner actually)
-    #     # This way, we can verify we pick up our dirty toilet as a target
-    #     self.coworker.x = 5
-    #     self.coworker.y = 5
+        # In addtion to changing job, lets move the coworker somewhere within the level (top-right corner actually)
+        # This way, we can verify we pick up our dirty toilet as a target
+        self.coworker.x = 5
+        self.coworker.y = 5
 
-    #     self.coworker.check_needs()
-    #     self.assertEqual(self.coworker.satisfying, "work")
-    #     self.assertEqual(self.coworker.target, toilet)
+        self.coworker.check_needs()
+        self.assertEqual(self.coworker.satisfying, "work")
+        self.assertEqual(self.coworker.target, toilet)
 
-    # def test_bladder_trigger(self):
-    #     self.coworker.bladder = 0
-    #     self.coworker.tick_needs()
-    #     self.assertEqual(self.coworker.bladder, 100)
-    #     self.assertEqual(len(self.game.find_objs("Urine")), 1)
+    def test_bladder_trigger(self):
+        self.coworker.bladder = 0
+        self.coworker.tick_needs()
+        self.assertEqual(self.coworker.bladder, 100)
+        self.assertEqual(len(self.game.find_objs("Urine")), 1)
 
-    # def test_bowels_trigger(self):
-    #     self.coworker.bowels = 0
-    #     self.coworker.tick_needs()
-    #     self.assertEqual(self.coworker.bowels, 100)
-    #     self.assertEqual(len(self.game.find_objs("Poo")), 1)
+    def test_bowels_trigger(self):
+        self.coworker.bowels = 0
+        self.coworker.tick_needs()
+        self.assertEqual(self.coworker.bowels, 100)
+        self.assertEqual(len(self.game.find_objs("Poo")), 1)
 
-    # def test_fired_trigger(self):
-    #     self.assertTrue("Test" in [x.name for x in self.game.world_objs[ObjType.mob]])
-    #     self.coworker.work = 0
-    #     self.coworker.tick_needs()
-    #     self.assertTrue("Test" not in [x.name for x in self.game.world_objs[ObjType.mob]])
-    #     self.assertEqual(len(self.game.find_objs("Test")), 0)
-    #     self.assertEqual(len(self.game.find_objs("Corpse")), 1)
+    def test_fired_trigger(self):
+        self.assertTrue("Test" in [x.name for x in self.game.world_objs[ObjType.mob]])
+        self.coworker.work = 0
+        self.coworker.tick_needs()
+        self.assertTrue("Test" not in [x.name for x in self.game.world_objs[ObjType.mob]])
+        self.assertEqual(len(self.game.find_objs("Test")), 0)
+        self.assertEqual(len(self.game.find_objs("Corpse")), 1)
 
-    # def test_quit_trigger(self):
-    #     self.assertTrue("Test" in [x.name for x in self.game.world_objs[ObjType.mob]])
-    #     self.coworker.mood = 0
-    #     self.coworker.tick_needs()
-    #     self.assertTrue("Test" not in [x.name for x in self.game.world_objs[ObjType.mob]])
-    #     self.assertEqual(len(self.game.find_objs("Test")), 0)
-    #     self.assertEqual(len(self.game.find_objs("Corpse")), 1)
+    def test_quit_trigger(self):
+        self.assertTrue("Test" in [x.name for x in self.game.world_objs[ObjType.mob]])
+        self.coworker.mood = 0
+        self.coworker.tick_needs()
+        self.assertTrue("Test" not in [x.name for x in self.game.world_objs[ObjType.mob]])
+        self.assertEqual(len(self.game.find_objs("Test")), 0)
+        self.assertEqual(len(self.game.find_objs("Corpse")), 1)
